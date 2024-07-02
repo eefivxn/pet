da7b625eac0a92cc394253d5f69dd766ce124541
eefivxn <160568289+eefivxn@users.noreply.github.com>
2024-07-02 20:40:24 +0700

    pet

---
+++ pet1/pet1/src/Pet.java
@@ -0,0 +1,15 @@
+public class Pet {
+    String nama = "jeki";
+    int umur = 10;
+    String ras = "Pitbull";
+    String makanan = "Wet Food";
+    String warna = "putih";
+
+    public void displayPet() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Ras: " + ras);
+        System.out.println("Makanan: " + makanan);
+        System.out.println("Warna: " + warna);
+    }
+}
